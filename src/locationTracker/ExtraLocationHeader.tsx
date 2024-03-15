8d758cf4c087fe99932b829d422ca50de6a3f905
Xander DiGiovanni <37936141+YourAverageLink@users.noreply.github.com>
2024-03-15 16:09:20 -0500

    setup build

--- src/locationTracker/ExtraLocationHeader.tsx
+++
@@ -1,7 +0,0 @@
-export default function ExtraLocationHeader( { title, icon }:  { title: string, icon: string }) {
-    return (
-        <div style={{ width: '100%', borderBottom: '1px solid var(--scheme-text)' }}>
-            {icon} {title}
-        </div>
-    );
-}
--- src/utils/Promises.ts
+++
@@ -1,6 +0,0 @@
-/**
- * setTimeout as a Promise.
- */
-export function delay(ms: number) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-}
